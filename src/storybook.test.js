d39f892733be5b5694b30364e4dc55ff66577d02
arai <souweb22@gmail.com>
2021-02-07 12:59:14 +0900

    スナップショットテスト導入

--- src/storybook.test.js
+++ src/storybook.test.js
@@ -1,12 +1,16 @@
 // storybook.test.js
-import path from 'path'
-import initStoryshots from '@storybook/addon-storyshots';
-import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+// import path from 'path';
+// import initStoryshots from '@storybook/addon-storyshots';
+// import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 
-initStoryshots({
-  suite: 'Image storyshots',
-  test: imageSnapshot({
-    storybookUrl: `file://${path.resolve(__dirname, '..', 'storybook-static')}`, // Storybookをビルド出力先を指定
-  }),
-  configPath: path.resolve(__dirname, '..', '.storybook'),
-});
+// initStoryshots({
+//   suite: 'Image storyshots',
+//   test: imageSnapshot({
+//     storybookUrl: `file://${path.resolve(__dirname, '..', 'storybook-static')}`, // Storybookをビルド出力先を指定
+//   }),
+//   configPath: path.resolve(__dirname, '..', '.storybook'),
+// });
+// initStoryshots();
+
+import initStoryshots from '@storybook/addon-storyshots';
+initStoryshots();
