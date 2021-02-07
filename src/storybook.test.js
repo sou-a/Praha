0f4ed45463845f811a8ffd3823cf0c99ee0fd4f3
arai <souweb22@gmail.com>
2021-02-07 13:11:51 +0900

    ビジュアルリグレッションテスト導入

--- src/storybook.test.js
+++ src/storybook.test.js
@@ -1,16 +1,13 @@
-// storybook.test.js
-// import path from 'path';
-// import initStoryshots from '@storybook/addon-storyshots';
-// import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
-
-// initStoryshots({
-//   suite: 'Image storyshots',
-//   test: imageSnapshot({
-//     storybookUrl: `file://${path.resolve(__dirname, '..', 'storybook-static')}`, // Storybookをビルド出力先を指定
-//   }),
-//   configPath: path.resolve(__dirname, '..', '.storybook'),
-// });
-// initStoryshots();
-
+/* eslint-disable no-undef */
+import path from 'path';
 import initStoryshots from '@storybook/addon-storyshots';
+import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
+
+initStoryshots({
+  suite: 'Image storyshots',
+  test: imageSnapshot({
+    storybookUrl: `file://${path.resolve(__dirname, '..', 'storybook-static')}`, // Storybookをビルド出力先を指定
+  }),
+  configPath: path.resolve(__dirname, '..', '.storybook'),
+});
 initStoryshots();
